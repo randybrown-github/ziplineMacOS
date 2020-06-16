5eec3019bf3585e3b6e05f85cd00324bb22d814e
randybrown-github <randybrown18@me.com>
2020-06-16 17:13:29 +0800

    zipline set-up on mac

---
+++ lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/var/root/anaconda3/envs/env_zipline/lib"
+XML2_LIBS="-lxml2 -L/var/root/anaconda3/envs/env_zipline/lib -lz -L/var/root/anaconda3/envs/env_zipline/lib -llzma -lpthread -L/var/root/anaconda3/envs/env_zipline/lib -liconv -L/var/root/anaconda3/envs/env_zipline/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/var/root/anaconda3/envs/env_zipline/include/libxml2 -I/var/root/anaconda3/envs/env_zipline/include"
+MODULE_VERSION="xml2-2.9.10"
+
---
+++ lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/var/root/anaconda3/envs/env_zipline/lib"
+XSLT_LIBS="-lxslt -L/var/root/anaconda3/envs/env_zipline/lib -lxml2 -L/var/root/anaconda3/envs/env_zipline/lib -L/var/root/anaconda3/envs/env_zipline/lib -lpthread -L/var/root/anaconda3/envs/env_zipline/lib -liconv -L/var/root/anaconda3/envs/env_zipline/lib -licui18n -licuuc -licudata -lm "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/var/root/anaconda3/envs/env_zipline/include"
+MODULE_VERSION="xslt-1.1.34"
